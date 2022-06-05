608bd3dbfea3133fb33bf6772607a8b504f2aad3
believemaster <believemasters@gmail.com>
2022-06-05 20:07:07 +0530

    Request body add a payload to sent data with request

--- githubapi.php
+++ githubapi.php
@@ -7,12 +7,20 @@ $headers = [
   // "User-Agent: believemaster"    // or passing header in another way below
 ];
 
+$payload = json_encode([
+  "name" => "Created from API",
+  "description" => "And example API-created repo"
+]);
+
 curl_setopt_array($ch, [
-  CURLOPT_URL => "https://api.github.com/user/starred/httpie/httpie",    // github API to check the repo is star or not
+  CURLOPT_URL => "https://api.github.com/user/repos",    // github API to check the repo is star or not
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_HTTPHEADER => $headers,
   CURLOPT_USERAGENT => "believemaster",
-  CURLOPT_CUSTOMREQUEST => "DELETE"     // request method to change the method to get a different result. In this case remove star from repo
+  // CURLOPT_CUSTOMREQUEST => "DELETE",     // request method to change the method to get a different result. In this case remove star from repo
+  // CURLOPT_CUSTOMREQUEST => "POST",     // this will create a new repo in the users github
+  CURLOPT_POST => true,               // another way to request POST method (this is also not required as payload is set)
+  CURLOPt_POSTFIELDS => $payload
 ]);
 
 $response = curl_exec($ch);
