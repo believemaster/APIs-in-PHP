345e48e54ab15d830c6a66203f35ad7ffd0ef409
believemaster <believemasters@gmail.com>
2022-06-05 17:52:35 +0530

    Getting response code HTTP status code

--- curlusage.php
+++ curlusage.php
@@ -8,12 +8,18 @@ $ch = curl_init();
 // or do it in this way:
 
 curl_setopt_array($ch, [
-  CURLOPT_URL => "https://randomuser.me/api",
+  // CURLOPT_URL => "https://randomuser.me/api", // random user API
+  CURLOPT_URL => "http://api.openweathermap.org/geo/1.0/direct?q=Delhi,110043&appid=226c1b54241af607992f7138913862d2",    // openweather API
   CURLOPT_RETURNTRANSFER => true
 ]);
 
 $response = curl_exec($ch);
 
+// Getting status code:
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
 curl_close($ch);
 
-echo $response, "\n";
+echo "Status Code: ", $status_code, "\n";
+
+echo "Response: ", $response, "\n";
