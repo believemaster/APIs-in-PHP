ec49e3ce4b9ffaf461c604c3ef8831fad94c2122
believemaster <believemasters@gmail.com>
2022-06-05 19:18:09 +0530

    Using github repo to test the specific headers requirement

---
+++ githubapi.php
@@ -0,0 +1,25 @@
+<?php
+
+$ch = curl_init();
+
+$headers = [
+  "Authorization: token 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",
+  // "User-Agent: believemaster"    // or passing header in another way below
+];
+
+curl_setopt_array($ch, [
+  CURLOPT_URL => "https://api.github.com/user/starred/httpie/httpie",    // github API to check the repo is star or not
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HTTPHEADER => $headers,
+  CURLOPT_USERAGENT => "believemaster"
+]);
+
+$response = curl_exec($ch);
+
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+echo "Status Code: ", $status_code, "\n";
+
+echo "Response: ", $response, "\n";
